d87cb36217bcb54be826b00ee710b95cd47370e8
leo <leoleitao1227@gmail.com>
2023-08-21 17:28:30 -0300

    att

---
+++ dom/desafio1/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Ana',
+        idade: 28,
+        imagem: 'https://cbissn.ibict.br/images/phocagallery/galeria2/thumbs/phoca_thumb_l_image03_grd.png'
+    }
+})
